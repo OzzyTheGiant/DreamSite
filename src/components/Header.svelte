9c2b1c0187e4422e49ba9a9f70679bbfeb82be5e
Oziel Perez <OZLperez11@gmail.com>
2023-09-27 17:20:02 -0500

    Added Header and Headline components to home page

---
+++ src/components/Header.svelte
@@ -0,0 +1,43 @@
+<header>
+  <Navbar 
+    let:hidden 
+    let:toggle 
+    navClass="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800"
+    navDivClass="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
+    fluid={false}>
+
+    <NavBrand href="/">
+      <img src="/images/logo-dark.svg" class="mr-3 h-5" alt="DreamCraft Logo"/>
+    </NavBrand>
+
+    <div class="lg:hidden lg:order-2">
+      <NavHamburger {btnClass} on:click={toggle}/>
+    </div>
+
+    <NavUl
+      {hidden}
+      divClass="justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+      ulClass="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 p-2">
+      
+      {#each Object.entries(links) as [link, text]}
+        <NavLi href={link}>{text}</NavLi>
+      {/each}
+    </NavUl>
+  </Navbar>
+</header>
+
+<script lang="ts">
+import Navbar from "flowbite-svelte/Navbar.svelte"
+import NavBrand from "flowbite-svelte/NavBrand.svelte"
+import NavHamburger from "flowbite-svelte/NavHamburger.svelte"
+import NavUl from "flowbite-svelte/NavUl.svelte"
+import NavLi from "flowbite-svelte/NavLi.svelte"
+
+export let links: { [key:string]: string }
+
+const btnClass = `
+  inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 
+  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 
+  dark:focus:ring-gray-600
+`
+</script>
---
+++ src/components/Headline.svelte
@@ -0,0 +1,83 @@
+<Section name={sectionType}>
+  {#if sectionType === "heroVisual"}
+  <div class="mr-auto place-self-center lg:col-span-7">
+    <HeroHeader h1Class={h1Class} pClass={pClass}>
+
+      <svelte:fragment slot="h1">{title}</svelte:fragment>
+      <svelte:fragment slot="paragraph">
+        <span class="text-2xl">{description}</span>
+      </svelte:fragment>
+
+      <a href={primaryButtonLink} class="mb-2">
+        <Button size="xl" color="primary" class="inline-flex items-center justify-center mr-3">
+          {primaryButtonText}<FileInvoiceSolid size="md" class="ml-2 -mr-1" />
+        </Button>
+      </a>
+
+      <a href={secondaryButtonLink} class="mb-2">
+        <Button size="xl" class="inline-flex items-center justify-center neutral">
+          {secondaryButtonText}
+        </Button>
+      </a>
+    </HeroHeader>
+  </div>
+  <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+    <img src={image} alt={imageAlt}/>
+  </div>
+  {:else if sectionType === "heroDefault"}
+    <HeroHeader>
+      <svelte:fragment slot="h1">{title}</svelte:fragment>
+      <svelte:fragment slot="paragraph">
+        <span class="text-2xl">{description}</span>
+      </svelte:fragment>
+    </HeroHeader>
+
+    <div class="button-group">
+      <a href={primaryButtonLink} class="mb-2">
+        <Button size="lg" color="primary">
+          {primaryButtonText}<FileInvoiceSolid size="md" class="ml-2 -mr-1"/>
+        </Button>
+      </a>
+      <a href={secondaryButtonLink} class="mb-2">
+        <Button size="lg" class="neutral">{secondaryButtonText}</Button>
+      </a>
+    </div>
+  {/if}
+</Section>
+
+<script lang="ts">
+import Section from "flowbite-svelte-blocks/Section.svelte"
+import HeroHeader from "flowbite-svelte-blocks/hero/HeroHeader.svelte"
+import Button from "flowbite-svelte/Button.svelte"
+import FileInvoiceSolid from "flowbite-svelte-icons/FileInvoiceSolid.svelte"
+
+export let sectionType: Section["name"]
+export let title: string
+export let description: string
+export let image: string
+export let imageAlt: string
+export let primaryButtonText: string
+export let secondaryButtonText: string
+export let primaryButtonLink: string = "/"
+export let secondaryButtonLink: string = "/"
+
+const h1Class = `
+  text-4xl font-extrabold tracking-tight leading-none max-w-2xl mb-4 md:text-5xl 
+  xl:text-6xl  dark:text-white
+`
+
+const pClass = `
+  max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400
+`
+</script>
+
+<style global lang="postcss">
+.button-group {
+  @apply flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row;
+  @apply sm:justify-center sm:space-y-0 sm:space-x-4;
+}
+
+.neutral {
+  @apply bg-neutral-500 hover:bg-neutral-600 focus:ring-neutral-300  text-white;
+}
+</style>
