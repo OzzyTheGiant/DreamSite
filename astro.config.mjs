7fa6135202bd2c59cad5625a399b952e470ce6c7
Oziel Perez <OZLperez11@gmail.com>
2023-09-27 14:46:35 -0500

    Installed dotenv library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
+import dotenv from "dotenv"
 import i18n from "./src/services/i18n"
+
+dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
