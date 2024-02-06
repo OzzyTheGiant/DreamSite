8df8706c72f7569263be596e402658c8927f351f
Oziel Perez <OZLperez11@gmail.com>
2024-02-06 00:55:18 -0600

    Added SocialMediaButtons and TaxonomyLists components

---
+++ src/components/SocialMediaButtons.svelte
@@ -0,0 +1,41 @@
+<span class="text-white inline-flex">
+  <a 
+    itemscope 
+    itemtype="http://schema.org/ShareAction" 
+    rel="me" 
+    target="_blank" 
+    class="facebook align-middle px-2 py-1 flex items-center mr-2"
+    aria-label="Share on Facebook"
+    href={`https://www.facebook.com/sharer/sharer.php?u=${articleURI}&title=${articleTitle}`}
+    data-href={articleURI}>
+    <Fa class="w-4 h-4 mr-2" icon={faFacebook}/>
+    Share
+  </a>
+  <a 
+    itemscope
+    itemtype="http://schema.org/ShareAction" 
+    rel="me" 
+    target="_blank" 
+    class="twitter align-middle px-2 py-1 flex items-center mr-2"
+    aria-label="Share on Twitter"
+    href={`https://www.twitter.com/intent/tweet?text=${articleTitle}&url=${articleURI}`}>
+    <Fa class="w-4 h-4 mr-2" icon={faTwitter}/>
+    Tweet
+  </a>
+</span>
+
+<script lang="ts">
+import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import Fa from "svelte-fa"
+
+export let title: string
+export let uri: string
+
+$: articleTitle = encodeURIComponent(title)
+$: articleURI =encodeURIComponent(uri)
+</script>
+
+<style>
+.facebook { background-color: #3B5998; }
+.twitter { background-color: #00ACED; }
+</style>
---
+++ src/components/TaxonomyLists.svelte
@@ -0,0 +1,41 @@
+<div class="text-dark {$$props.class}">
+  {#if categories.length}
+    <div class="flex items-top w-full dark:text-accent-300">
+      <Star class="h-8 mr-2 {dark ? 'text-white' : ''}"/>
+      <ul class="mt-0 mb-4">
+        {#each categories as category}
+          <li 
+            class="bg-main inline-block px-2 py-1 mr-2 mb-1 rounded-lg" 
+            class:bg-white={dark}>
+            {category.name}
+          </li>
+        {/each}
+      </ul>
+    </div>
+  {/if}
+
+  {#if tags.length}
+  <div class="flex items-top w-full dark:text-accent-300">
+      <Tag class="h-8 mr-2 {dark ? 'text-white': ''}"/>
+      <ul class="mt-0 mb-4">
+          {#each tags as tag}
+          <li 
+              class="bg-main inline-block px-2 py-1 mr-2 mb-1 rounded-lg" 
+              class:bg-white={dark}>
+              {tag.name}
+          </li>
+          {/each}
+      </ul>
+  </div>
+  {/if}
+</div>
+
+<script lang="ts">
+import type { ProductCategory, ProductTag } from "@/models/Product"
+import Star from "flowbite-svelte-icons/StarSolid.svelte"
+import Tag from "flowbite-svelte-icons/TagSolid.svelte"
+
+export let categories: ProductCategory[]
+export let tags: ProductTag[]
+export let dark: boolean = false
+</script>
